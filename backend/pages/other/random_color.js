742a93baee28dab88090e8bb64337bb678a19db1
realredtext <N7AIA888@gmail.com>
2023-02-17 09:31:38 -0500

    random color is less ugly

--- backend/pages/other/random_color.js
+++ backend/pages/other/random_color.js
@@ -1,3 +1,4 @@
 module.exports.GET = async function(req, serve, vars, evars) {
-	serve("#" + ("00000" + Math.floor(Math.random() * 16777215).toString(16)).slice(-6).toUpperCase());
+	var rand = "#" + ("00000" + Math.floor(Math.random() * 16777215).toString(16)).slice(-6).toUpperCase()
+	serve("<div style=\"width: 500px; height: 500px; background-color:"+rand+";\"></div><span>"+rand+"</span>");
 }
